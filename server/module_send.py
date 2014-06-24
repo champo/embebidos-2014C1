a2231db390a568ceb751a06b9b9a5491e4d2276f
Esteban Ordano <eordano@gmail.com>
2014-06-24 12:12:50 -0300

    Fixes send value to module

--- server/module_send.py
+++ server/module_send.py
@@ -10,14 +10,25 @@ def main(args):
         return usage()
     i2c_address = int(args[0])
     value_to_send = int(args[1])
-    ser = serial.Serial(DEVICE, SPEED, timeout = 1)
-    ser.write(struct.pack('BBB', i2c_address, 1, value_to_send))
+
+    ser = serial.Serial()
+    ser.port = DEVICE
+    ser.baudrate = SPEED
+    ser.open()
+
+# Wait for the arduino to reset
+    time.sleep(2)
+
+    ser.write([i2c_address])
+    ser.write([0])
+    ser.write([value_to_send])
+
     ser.flush()
     ser.close()
 
 def usage():
     print ('Usage: \n\t%s i2c_address value\n\t'
-        'Send a get to the module on the specified i2c address.')
+        'Send a PUT with the given value to the module on the specified i2c address.')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
